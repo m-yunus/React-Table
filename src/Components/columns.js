4465b6a55f5717451c795f1e7e978b6e8493db5d
Yunus <sanuyoonus@gmail.com>
2023-10-27 14:49:27 +0530

    added table

---
+++ src/Components/columns.js
@@ -0,0 +1,28 @@
+export const COLUMNS=[
+    {
+        Header:'Id',
+        accessor: 'id',
+    },
+    {
+        Header:'First Name',
+        accessor: 'first_name',
+    },
+    {
+        Header:'Last Name',
+        accessor: 'last_name',
+    },
+    {
+        Header:'Email',
+        accessor: 'email',
+    },
+    {
+        Header:'Date of Birth',
+        accessor: 'date_of_birth'
+    },
+    {
+        Header:'Ip Address',
+        accessor: 'ip_address',
+    },
+  
+    
+]
