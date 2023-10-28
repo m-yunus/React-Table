ebcc82e33b85022316dc6766fc7f5a07672d1307
Yunus <sanuyoonus@gmail.com>
2023-10-28 12:31:58 +0530

    added the header groups

--- src/Components/BasicTable.js
+++ src/Components/BasicTable.js
@@ -1,59 +1,59 @@
 import React, { useMemo } from "react";
 import { useTable } from "react-table";
 import MOCK_DATA from "./MOCK_DATA.json";
-import { COLUMNS } from "./columns";
+import { COLUMNS, GROUPED_COLUMNS } from "./columns";
+
+
 const BasicTable = () => {
-  const columns = useMemo(() => COLUMNS, []);
+  const columns = useMemo(() => GROUPED_COLUMNS, []);
   const data = useMemo(() => MOCK_DATA, []);
   const tableInstance = useTable({
     columns,
     data,
   });
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+  const { getTableProps, getTableBodyProps, headerGroups,footerGroups, rows, prepareRow } =
     tableInstance;
+
   return (
-    <>
-      <table {...getTableProps()}>
-        <thead>
+    <div className="table-container">
+      <table {...getTableProps()} className="custom-table">
+        <thead className="table-header">
+          {headerGroups.map((headergroup) => (
+            <tr {...headergroup.getHeaderGroupProps()}>
+              {headergroup.headers.map((column) => (
+                <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()} className="table-body">
+          {rows.map((row) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+        <tfoot className="table-header">
           {
-            headerGroups.map((headergroup)=>(
-              <tr {...headergroup.getHeaderGroupProps()}>
-                {
-                  headergroup.headers.map(column=>(
-                    <th {...column.getHeaderProps()}>{column.render('Header')}</th>
-                  ))
-                }
+            footerGroups.map(footergroup=>(
+              <tr {...footergroup.getFooterGroupProps()}>
+                    {
+                      footergroup.headers.map(column=>(
+                        <td {...column.getFooterProps()}>{column.render('Footer')}</td>
+                        ))
+                    }
               </tr>
             ))
           }
-          <tr>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody {...getTableBodyProps()}>
-          {
-            rows.map(row =>{
-              prepareRow(row)
-              return (
-                <tr {...row.getRowProps()}>
-                  {
-                    row.cells.map(cell=>{
-                      return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-                    })
-                  }
-                  <td></td>
-                </tr>
-              )
-            })
-          }
-          <tr>
-            <td></td>
-          </tr>
-        </tbody>
+        </tfoot>
       </table>
-    </>
+    </div>
   );
 };
 
 export default BasicTable;
- 
--- src/Components/columns.js
+++ src/Components/columns.js
@@ -1,28 +1,81 @@
 export const COLUMNS=[
     {
         Header:'Id',
+        Footer:'Id',
         accessor: 'id',
     },
     {
         Header:'First Name',
+        Footer:'First Name',
         accessor: 'first_name',
     },
     {
         Header:'Last Name',
+        Footer:'Last Name',
         accessor: 'last_name',
     },
     {
         Header:'Email',
+        Footer:'Email',
         accessor: 'email',
     },
     {
         Header:'Date of Birth',
+        Footer:'Date of Birth',
         accessor: 'date_of_birth'
     },
     {
         Header:'Ip Address',
+        Footer:'Ip Address',
         accessor: 'ip_address',
     },
   
     
+]
+
+export const GROUPED_COLUMNS =[
+    {
+        Header:'Id',
+        Footer:'Id',
+        accessor: 'id',
+    },
+    {
+        Header:'Name',
+        Footer:'Name',
+        columns:[
+            {
+                Header:'First Name',
+                Footer:'First Name',
+                accessor: 'first_name',
+            },
+            {
+                Header:'Last Name',
+                Footer:'Last Name',
+                accessor: 'last_name',
+            },
+        ],
+
+    },
+    {
+        Header:"Info",
+        Footer:"Info",
+        columns:[
+            {
+                Header:'Email',
+                Footer:'Email',
+                accessor: 'email',
+            },
+            {
+                Header:'Date of Birth',
+                Footer:'Date of Birth',
+                accessor: 'date_of_birth'
+            },
+            {
+                Header:'Ip Address',
+                Footer:'Ip Address',
+                accessor: 'ip_address',
+            },
+        ]
+
+    },
 ]
